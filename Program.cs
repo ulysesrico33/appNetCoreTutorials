7023f837cb08118fbd1d6a708618dcc73ef4ece9
Ulyses <ulyses.rico.2012@gmail.com>
2021-04-19 11:36:13 -0500

    ...

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace appNetCoreTutorials
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
