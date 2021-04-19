f7ae43757157ec13016c05da1840d16521b66683
Ulyses <ulyses.rico.2012@gmail.com>
2021-04-19 12:37:19 -0500

    ...

--- Program.cs
+++ Program.cs
@@ -1,12 +1,23 @@
 ﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace appNetCoreTutorials
 {
     class Program
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
+
+        public static void Main(){
+           Thread.CurrentThread.Name = "Main";
+
+            // Create a task and supply a user delegate by using a lambda expression.
+            Task taskA = new Task( () => Console.WriteLine("Hello from taskA."));
+            // Start the task.
+            taskA.Start();
+
+             // Output a message from the calling thread.
+             Console.WriteLine("Hello from thread '{0}'.",Thread.CurrentThread.Name);
+               taskA.Wait();
         }
     }
 }
