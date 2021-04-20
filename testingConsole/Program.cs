d61d28f76d6467ce855df803c9e08d8a1dd71811
Ulyses <ulyses.rico.2012@gmail.com>
2021-04-20 11:18:03 -0500

    ...

---
+++ testingConsole/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace testingConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
